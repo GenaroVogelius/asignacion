0c46458b40cb9ec8de7dfe5ef2f6f40adbf121d3
LEO <LEO@DESKTOP-3798V6K>
2023-07-10 14:52:13 -0300

    unittest add

---
+++ asignacion/Tests/EncriptadorTest.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using asignacion.Services;
+
+namespace asignacion.Tests
+{
+        public class PasswordServiceTests
+        {
+            [Fact]
+            public void EncryptPassword_ReturnsEncryptedPassword()
+            {
+                // Arrange
+                var passwordService = new PasswordService();
+                string password = "password123";
+
+                // Act
+                string encryptedPassword = passwordService.EncryptPassword(password);
+
+                // Assert
+                Assert.NotNull(encryptedPassword);
+                Assert.NotEmpty(encryptedPassword);
+                Assert.NotEqual(password, encryptedPassword);
+            }
+        }
+    }
